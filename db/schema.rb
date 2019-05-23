21a55f3eb5bf5ea8dcf9e498b99b1d45e314a61c
trillianjose <trillianjosea@gmail.com>
2019-05-23 11:48:31 +0200

    adding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_133041) do
+ActiveRecord::Schema.define(version: 2019_05_23_094720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_05_21_133041) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
+    t.bigint "posts_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -27,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_05_21_133041) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +43,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_133041) do
     t.string "password_digest"
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "posts", "users"
 end
